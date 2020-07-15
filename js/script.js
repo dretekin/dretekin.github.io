de3ec8ec1f5b30ce4b612a577ad690d185f85a06
dretekin <dretekin@gmail.com>
2020-07-15 11:21:43 +0000

    mobile and desktop nav and cart display remaining

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-let currencyWrapper = document.querySelector(".currency-convertor-wrapper");
-
+let currencyWrapper = document.getElementById("currency-convertor-wrapper");
 let selectorBtn = currencyWrapper.querySelector(".selector-btn");
-
 let currencies = currencyWrapper.querySelector("#currencies");
-
 let selectedCurrency = currencyWrapper.querySelector(".selected");
+let lMenu = document.getElementById("l-menu");
+let search = lMenu.querySelector(".search");
+let searchBoxWrapper = lMenu.querySelector(".search-box-wrapper");
+let closeSearchBox = lMenu.querySelector(".close-seach-box");
 
 selectorBtn.addEventListener("click", (e) => {
   currencies.classList.toggle("dropdown");
@@ -21,4 +22,12 @@ window.addEventListener("click", (e) => {
     e.target.classList.contains("currency-selector")
   )
     currencies.classList.remove("dropdown");
+});
+
+search.addEventListener("click", function (e) {
+  if (this.contains(e.target)) searchBoxWrapper.classList.add("show");
+});
+
+closeSearchBox.addEventListener("click", function () {
+  searchBoxWrapper.classList.remove("show");
 });
