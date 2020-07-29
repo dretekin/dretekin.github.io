6e2236a7fb9825310ef83571764d48f5a15cbd5c
dretekin <dretekin@gmail.com>
2020-07-29 19:25:15 +0000

    integrate mobile menu

--- js/script.js
+++ js/script.js
@@ -41,3 +41,76 @@ window.onload = function () {
 
   // if (mobileNav.clientHeight < viewPortHeight) mobileNav.style.height = "100vh";
 };
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+// document.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
+
+let container = document.querySelector(".container"),
+  containerCover = document.querySelector(".container-wrapper"),
+  containerCoverDark = document.querySelector(".container-wrapperCover"),
+  showNav = document.querySelector(".menu-navBtn"),
+  showSubNav = document.querySelectorAll(".nav-link_toSubNav"),
+  subNav = document.querySelector(".sub-nav"),
+  goBack = document.querySelectorAll(".nav-link_goBack");
+
+// let viewPortWidth =
+//   window.innerWidth ||
+//   document.documentElement.clientWidth ||
+//   document.body.clientWidth;
+// // let moveAmt = (80 / 100) * viewPortWidth original;
+let translateXNav = 0;
+let moveAmt = 300;
+let time = 0.8;
+
+showNav.addEventListener("click", function () {
+  translateXNav += moveAmt;
+  gsap.to(".nav", { duration: 0.4, ease: "power1.out", x: translateXNav });
+  gsap.to(".container-wrapper", {
+    duration: 0.4,
+    ease: "power1.out",
+    x: moveAmt,
+    onComplete: function () {
+      containerCoverDark.style.display = "block";
+    },
+  });
+});
+
+containerCoverDark.addEventListener("click", function () {
+  translateXNav -= moveAmt;
+  gsap.to(".nav", { duration: 0.4, ease: "power1.out", x: translateXNav });
+  gsap.to(".container-wrapper", {
+    duration: 0.4,
+    ease: "power1.out",
+    x: 0,
+    onComplete: () => {
+      this.style.display = "none";
+    },
+  });
+});
+
+showSubNav.forEach(function (subNav) {
+  subNav.addEventListener("click", function () {
+    this.nextElementSibling.style.display = "block";
+    translateXNav += moveAmt;
+    gsap.to(".nav", { duration: 0.6, ease: "power1.out", x: translateXNav });
+  });
+});
+
+goBack.forEach(function (el) {
+  el.addEventListener("click", function () {
+    translateXNav -= moveAmt;
+    gsap.to(".nav", {
+      duration: 0.6,
+      ease: "power1.out",
+      x: translateXNav,
+      onComplete: function () {
+        el.parentElement.parentElement.style.display = "none";
+      },
+    });
+  });
+});
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
