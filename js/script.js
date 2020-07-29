6624c6894f0e686ba43788b94dde81f741935f4c
dretekin <dretekin@gmail.com>
2020-07-29 12:39:12 +0000

    BEMing 80% done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-let currencyWrapper = document.getElementById("currency-convertor-wrapper");
-let selectorBtn = currencyWrapper.querySelector(".selector-btn");
-let currencies = currencyWrapper.querySelector("#currencies");
-let selectedCurrency = currencyWrapper.querySelector(".selected");
+let currency = document.querySelector(".currency");
+let currencySelectBtn = document.querySelector(".currency-selectBtn");
+let currencyItems = document.querySelector(".currency-items");
+let currencySelected = document.querySelector(".currency-selected");
 let lMenu = document.getElementById("l-menu");
 let search = lMenu.querySelector(".search");
 let searchBoxWrapper = lMenu.querySelector(".search-box-wrapper");
 let closeSearchBox = lMenu.querySelector(".close-seach-box");
-let mobileNav = document.querySelector(".mobile-nav");
+// let mobileNav = document.querySelector(".mobile-nav");
 
-selectorBtn.addEventListener("click", (e) => {
-  currencies.classList.toggle("dropdown");
+currencySelectBtn.addEventListener("click", (e) => {
+  currencyItems.classList.toggle("dropdown");
 });
 
-currencies.addEventListener("click", (e) => {
-  selectedCurrency.textContent = e.target.dataset.currency;
-  currencies.classList.remove("dropdown");
+currencyItems.addEventListener("click", (e) => {
+  currencySelected.textContent = e.target.dataset.currency;
+  currencyItems.classList.remove("dropdown");
 });
 
 window.addEventListener("click", (e) => {
   if (
-    !currencyWrapper.contains(e.target) ||
+    !currency.contains(e.target) ||
     e.target.classList.contains("currency-selector")
   )
-    currencies.classList.remove("dropdown");
+    currencyItems.classList.remove("dropdown");
 });
 
 search.addEventListener("click", function (e) {
@@ -39,5 +39,5 @@ window.onload = function () {
     document.documentElement.clientHeight ||
     document.body.clientHeight;
 
-  if (mobileNav.clientHeight < viewPortHeight) mobileNav.style.height = "100vh";
+  // if (mobileNav.clientHeight < viewPortHeight) mobileNav.style.height = "100vh";
 };
